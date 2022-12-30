d5fcab4e5874e2d3ce44c4af71bd77abc8cb7cff
midnight <midnight@midnights-MacBook-Air.local>
2022-12-30 17:50:38 +0800

    latested

--- twitch/settings.py
+++ twitch/settings.py
@@ -57,7 +57,9 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = BASE_DIR, 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- zero/views.py
+++ zero/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'home.html')
 
-def aboutus(request):
-    return HttpResponse('Hello World')
+def about(request):
+    return render(request,'about.html')
 
 def Shop(request):
     return HttpResponse('Hello World')
